c2d31f00e81905231b0ea30beb6c9e54162943a3
ofek <benavrahamofek@gmail.com>
2024-03-24 23:31:15 +0200

    add app page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Navbar from './Navbar';
+import Home from './Home';
+import Explore from './Explore';
+import About from './About';
+import Forum from './Forum';
+import CreatePost from './CreatePost';
+import ShowPost from './ShowPost';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <CreatePost />
+      <About />
+      <Explore />
+      <Home />
+      <ShowPost />
     </div>
   );
 }
