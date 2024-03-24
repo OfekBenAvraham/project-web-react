abeda651181ea18fc6b6b0064aa1c545dbb88ff3
ofek <benavrahamofek@gmail.com>
2024-03-24 23:31:26 +0200

    explore page

---
+++ src/Explore.js
@@ -0,0 +1,59 @@
+import React, { useEffect, useState } from 'react';
+
+const Explore = () => {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    fetch('https://project-web-psi.vercel.app/category', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setCategories(data);
+      })
+      .catch((error) => console.error('Error fetching categories:', error));
+  }, []);
+
+  return (
+    <div className="bg-white dark:bg-gray-800 h-screen h-full py-6 sm:py-8 lg:py-12">
+      <div className="mx-auto max-w-screen-2xl px-4 md:px-8">
+        <div className="mb-4 flex items-center justify-between gap-8 sm:mb-8 md:mb-12">
+          <div className="flex items-center gap-12">
+            <h2 className="text-2xl font-bold text-gray-800 lg:text-3xl dark:text-white">
+              Explore
+            </h2>
+
+            <p className="hidden max-w-screen-sm text-gray-500 dark:text-gray-300 md:block">
+              Here you can get inspired, see other's works and get ideas for
+              your next DIY project!
+            </p>
+          </div>
+        </div>
+        <div id="categoryContainer" className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+          {categories.map((category) => (
+            <a
+              key={category._id}
+              href="./forum.html"
+              className="group relative flex h-48 items-end overflow-hidden rounded-lg bg-gray-100 shadow-lg md:h-80"
+            >
+              <img
+                src={category.imageUrl}
+                alt="Category"
+                className="absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110"
+              />
+              <div className="pointer-events-none absolute inset-0 bg-gradient-to-t from-gray-800 via-transparent to-transparent opacity-50"></div>
+              <span className="relative ml-4 mb-3 inline-block text-sm text-white md:ml-5 md:text-lg">
+                {category.name}
+              </span>
+            </a>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Explore;
