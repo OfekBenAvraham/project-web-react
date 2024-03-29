e717192fa59a349b2fd4cec08c08f5b466905cd2
gpariente <guypar1997@gmail.com>
2024-03-29 13:02:19 +0300

    routing from category to posts

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import React, { useState } from "react";
 import Navbar from "./Navbar";
 import Home from "./Home";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import LoginModal from './LoginModal';
-import SignupModal from './SignupModal';
+import LoginModal from "./LoginModal";
+import SignupModal from "./SignupModal";
 import Explore from "./Explore";
 import About from "./About";
+import Forum from "./Forum";
 
 function App() {
   const [isLoginModalOpen, setIsLoginModalOpen] = useState(false);
@@ -13,13 +14,23 @@ function App() {
 
   return (
     <BrowserRouter>
-      <Navbar onLoginClick={() => setIsLoginModalOpen(true)} onSignupClick={() => setIsSignupModalOpen(true)} />
-      <LoginModal isOpen={isLoginModalOpen} onClose={() => setIsLoginModalOpen(false)} />
-      <SignupModal isOpen={isSignupModalOpen} onClose={() => setIsSignupModalOpen(false)} />
+      <Navbar
+        onLoginClick={() => setIsLoginModalOpen(true)}
+        onSignupClick={() => setIsSignupModalOpen(true)}
+      />
+      <LoginModal
+        isOpen={isLoginModalOpen}
+        onClose={() => setIsLoginModalOpen(false)}
+      />
+      <SignupModal
+        isOpen={isSignupModalOpen}
+        onClose={() => setIsSignupModalOpen(false)}
+      />
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="about" element={<About />} />
         <Route path="explore" element={<Explore />}></Route>
+        <Route path="/forum/:categoryName" element={<Forum />} />
       </Routes>
     </BrowserRouter>
   );
