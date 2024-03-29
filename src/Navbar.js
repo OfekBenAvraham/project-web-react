593b2597fac6db9ff826594839fdc5f63b571f8b
ofek <benavrahamofek@gmail.com>
2024-03-29 13:32:09 +0300

    navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,5 +1,4 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState } from "react";
 
 const Navbar = ({ onLoginClick, onSignupClick }) => {
   const toggleDarkMode = () => {
@@ -29,13 +28,30 @@ const Navbar = ({ onLoginClick, onSignupClick }) => {
         <ul
           className={`${
             isOpen ? "flex" : "hidden"
-          } items-center justify-center gap-6 md:flex flex-col md:flex-row`}
+          } items-center justify-end gap-6 md:flex flex-col md:flex-row`}
         >
           <li className="pt-1.5 font-dm text-sm font-medium text-slate-700 dark:text-slate-200">
             <a href="./explore">Explore</a>
           </li>
           <li className="pt-1.5 font-dm text-sm font-medium text-slate-700 dark:text-slate-200">
             <a href="./about">About</a>
+          </li>
+          <li className="pt-1.5 font-dm text-sm font-medium text-slate-700 dark:text-slate-200 md:hidden">
+            <a href="#" onClick={onLoginClick}>
+              Login
+            </a>
+          </li>
+          <li className="pt-1.5 font-dm text-sm font-medium text-slate-700 dark:text-slate-200 md:hidden">
+            <a href="#" onClick={onSignupClick} className="rounded-md bg-gradient-to-br from-orange-600 to-orange-400 px-3 py-1.5 font-dm text-sm font-medium text-white shadow-md transition-transform duration-200 ease-in-out hover:scale-[1.03]">
+              Sign up
+            </a>
+          </li>
+          <li className="pt-1.5 font-dm text-sm font-medium text-slate-700 dark:text-slate-200 md:hidden">
+            <button onClick={toggleDarkMode} className="bg-transparent border-none cursor-pointer">
+              {/* Dark mode toggle icons for mobile view */}
+              <svg className="dark:hidden" width="16" height="16" xmlns="http://www.w3.org/2000/svg"><path className="fill-slate-300" d="M7 0h2v2H7zM12.88 1.637l1.414 1.415-1.415 1.413-1.413-1.414zM14 7h2v2h-2zM12.95 14.433l-1.414-1.413 1.413-1.415 1.415 1.414zM7 14h2v2H7zM2.98 14.364l-1.413-1.415 1.414-1.414 1.414 1.415zM0 7h2v2H0zM3.05 1.706 4.463 3.12 3.05 4.535 1.636 3.12z"/><path className="fill-slate-400" d="M8 4C5.8 4 4 5.8 4 8s1.8 4 4 4 4-1.8 4-4-1.8-4-4-4Z"/></svg>
+              <svg className="hidden dark:block" width="16" height="16" xmlns="http://www.w3.org/2000/svg"><path className="fill-slate-400" d="M6.2 1C3.2 1.8 1 4.6 1 7.9 1 11.8 4.2 15 8.1 15c3.3 0 6-2.2 6.9-5.2C9.7 11.2 4.8 6.3 6.2 1Z"/><path className="fill-slate-500" d="M12.5 5a.625.625 0 0 1-.625-.625 1.252 1.252 0 0 0-1.25-1.25 .625.625 0 1 1 0-1.25 1.252 1.252 0 0 0 1.25-1.25 .625.625 0 1 1 1.25 0c.001.69.56 1.249 1.25 1.25a.625.625 0 1 1 0 1.25c-.69.001-1.249.56-1.25 1.25A.625.625 0 0 1 12.5 5Z"/></svg>
+            </button>
           </li>
         </ul>
         <div className="flex-grow"></div>
@@ -74,37 +90,10 @@ const Navbar = ({ onLoginClick, onSignupClick }) => {
             </svg>
           </button>
         </div>
-        <button onClick={toggleDarkMode}>
-          <svg
-            className="dark:hidden"
-            width="16"
-            height="16"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              className="fill-slate-300"
-              d="M7 0h2v2H7zM12.88 1.637l1.414 1.415-1.415 1.413-1.413-1.414zM14 7h2v2h-2zM12.95 14.433l-1.414-1.413 1.413-1.415 1.415 1.414zM7 14h2v2H7zM2.98 14.364l-1.413-1.415 1.414-1.414 1.414 1.415zM0 7h2v2H0zM3.05 1.706 4.463 3.12 3.05 4.535 1.636 3.12z"
-            />
-            <path
-              className="fill-slate-400"
-              d="M8 4C5.8 4 4 5.8 4 8s1.8 4 4 4 4-1.8 4-4-1.8-4-4-4Z"
-            />
-          </svg>
-          <svg
-            className="hidden dark:block"
-            width="16"
-            height="16"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              className="fill-slate-400"
-              d="M6.2 1C3.2 1.8 1 4.6 1 7.9 1 11.8 4.2 15 8.1 15c3.3 0 6-2.2 6.9-5.2C9.7 11.2 4.8 6.3 6.2 1Z"
-            />
-            <path
-              className="fill-slate-500"
-              d="M12.5 5a.625.625 0 0 1-.625-.625 1.252 1.252 0 0 0-1.25-1.25.625.625 0 1 1 0-1.25 1.252 1.252 0 0 0 1.25-1.25.625.625 0 1 1 1.25 0c.001.69.56 1.249 1.25 1.25a.625.625 0 1 1 0 1.25c-.69.001-1.249.56-1.25 1.25A.625.625 0 0 1 12.5 5Z"
-            />
-          </svg>
+        <button onClick={toggleDarkMode} className="hidden md:block">
+          {/* Dark mode toggle icons for desktop view */}
+          <svg className="dark:hidden" width="16" height="16" xmlns="http://www.w3.org/2000/svg"><path className="fill-slate-300" d="M7 0h2v2H7zM12.88 1.637l1.414 1.415-1.415 1.413-1.413-1.414zM14 7h2v2h-2zM12.95 14.433l-1.414-1.413 1.413-1.415 1.415 1.414zM7 14h2v2H7zM2.98 14.364l-1.413-1.415 1.414-1.414 1.414 1.415zM0 7h2v2H0zM3.05 1.706 4.463 3.12 3.05 4.535 1.636 3.12z"/><path className="fill-slate-400" d="M8 4C5.8 4 4 5.8 4 8s1.8 4 4 4 4-1.8 4-4-1.8-4-4-4Z"/></svg>
+          <svg className="hidden dark:block" width="16" height="16" xmlns="http://www.w3.org/2000/svg"><path className="fill-slate-400" d="M6.2 1C3.2 1.8 1 4.6 1 7.9 1 11.8 4.2 15 8.1 15c3.3 0 6-2.2 6.9-5.2C9.7 11.2 4.8 6.3 6.2 1Z"/><path className="fill-slate-500" d="M12.5 5a.625.625 0 0 1-.625-.625 1.252 1.252 0 0 0-1.25-1.25 .625.625 0 1 1 0-1.25 1.252 1.252 0 0 0 1.25-1.25 .625.625 0 1 1 1.25 0c.001.69.56 1.249 1.25 1.25a.625.625 0 1 1 0 1.25c-.69.001-1.249.56-1.25 1.25A.625.625 0 0 1 12.5 5Z"/></svg>
         </button>
       </nav>
     </header>
